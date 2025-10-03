e6fc23a9af43d5619aff3264ad08cde59cab2fef
EDNALDO BARROS DA SILVA <116734229+EdnaldoBarros@users.noreply.github.com>
2025-10-02 21:21:11 -0300

    Update calculadora.py
    Foi adicionado algumas funções a mais na calculadora.

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,12 @@
-
 def subtrair(numero1, numero2):
     return numero1 - numero2
+
 def dividir(numero1, numero2):
     return numero1 / numero2
+
 def multiplicar(numero1, numero2):
     return numero1 * numero2
+
 def somar(numero1, numero2):
     return numero1 + numero2
 
@@ -15,26 +17,28 @@ Calculadora:
 [3] - Multiplicar
 [4] - Somar
 ''')
-opcao = int(input('Digite uma opcao:'))
 
+opcao = int(input('Digite uma opção: '))
 
 numero1 = int(input("Digite o 1º número: "))
 numero2 = int(input("Digite o 2º número: "))
 
-if opcao == 1:    
+if opcao == 1:
     resultado = subtrair(numero1, numero2)
     print(f'{numero1} - {numero2} = {resultado}')
-
 elif opcao == 2:
-    resultado = dividir(numero1, numero2)
-    print(f'{numero1} / {numero2} = {resultado}')
-
+    if numero2 == 0:
+        print("Erro: divisão por zero!")
+    else:
+        resultado = dividir(numero1, numero2)
+        print(f'{numero1} / {numero2} = {resultado}')
 elif opcao == 3:
     resultado = multiplicar(numero1, numero2)
     print(f'{numero1} * {numero2} = {resultado}')
-
 elif opcao == 4:
     resultado = somar(numero1, numero2)
     print(f'{numero1} + {numero2} = {resultado}')
+else:
+    print("Opção inválida!")
 
 
