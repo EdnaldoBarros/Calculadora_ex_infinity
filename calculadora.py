f9cfac3a2a92cc036c4a12c4e0e91ddc3b185958
EDNALDO BARROS DA SILVA <116734229+EdnaldoBarros@users.noreply.github.com>
2025-10-02 21:18:20 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,40 @@
+
+def subtrair(numero1, numero2):
+    return numero1 - numero2
+def dividir(numero1, numero2):
+    return numero1 / numero2
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+def somar(numero1, numero2):
+    return numero1 + numero2
+
+print('''
+Calculadora:
+[1] - Subtrair
+[2] - Dividir
+[3] - Multiplicar
+[4] - Somar
+''')
+opcao = int(input('Digite uma opcao:'))
+
+
+numero1 = int(input("Digite o 1º número: "))
+numero2 = int(input("Digite o 2º número: "))
+
+if opcao == 1:    
+    resultado = subtrair(numero1, numero2)
+    print(f'{numero1} - {numero2} = {resultado}')
+
+elif opcao == 2:
+    resultado = dividir(numero1, numero2)
+    print(f'{numero1} / {numero2} = {resultado}')
+
+elif opcao == 3:
+    resultado = multiplicar(numero1, numero2)
+    print(f'{numero1} * {numero2} = {resultado}')
+
+elif opcao == 4:
+    resultado = somar(numero1, numero2)
+    print(f'{numero1} + {numero2} = {resultado}')
+
+
